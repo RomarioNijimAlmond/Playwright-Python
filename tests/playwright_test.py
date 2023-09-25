50c2e540a18c455ddf1ba38b58338399577d2faa
RomarioNijim <romario@almond-qms.com>
2023-09-25 18:27:25 +0300

    fix fixture

--- tests/playwright_test.py
+++ tests/playwright_test.py
@@ -1,15 +1,14 @@
 from playwright.sync_api import sync_playwright, Page, expect
 import pytest
 
-@pytest.fixture
-def page():
-   playwright = sync_playwright().start()
-   browser = playwright.chromium.launch(headless=False)
-   return browser.new_page()
+#runs before each function
+@pytest.fixture(autouse=True, scope="function")
+def go_to_page(page:Page):
+   page.goto("https://playwright.dev/python")
+   return page
    
 
 def test_page_has_get_started_link(page:Page):
-    page.goto("https://playwright.dev/python")
     get_started = page.get_by_role("link",name="Get started")
     get_started_visibility = get_started.is_visible()
-    assert  get_started_visibility == True
+    assert get_started_visibility == True
